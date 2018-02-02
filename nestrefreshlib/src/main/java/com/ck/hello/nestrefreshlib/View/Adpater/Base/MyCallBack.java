d19eaf037dc09a793652ad3926443b6ce42d6fe6
公司 <951827860@qq.com>
2018-02-02 11:08:13 +0800

    易用性修改 增加通用性强的BaseHolder

--- nestrefreshlib/src/main/java/com/ck/hello/nestrefreshlib/View/Adpater/Base/MyCallBack.java
+++ nestrefreshlib/src/main/java/com/ck/hello/nestrefreshlib/View/Adpater/Base/MyCallBack.java
@@ -1,8 +1,8 @@
-package com.ck.hello.nestrefreshlib.View.Adpater.Base
+package com.ck.hello.nestrefreshlib.View.Adpater.Base;
 
 /**
  * Created by 不听话的好孩子 on 2018/1/29.
  */
 interface MyCallBack<T> {
-    fun call(t:T)
+    void call(T t);
 }
